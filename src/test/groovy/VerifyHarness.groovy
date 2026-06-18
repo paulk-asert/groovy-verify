278c62ed4a56ac8dfcbffdff78ea9c589326f348
Paul King <paulk@asert.com.au>
2026-06-18 22:34:36 +1000

    @NonNull for a param

--- src/test/groovy/VerifyHarness.groovy
+++ src/test/groovy/VerifyHarness.groovy
@@ -39,6 +39,7 @@ class VerifyHarness {
 
     static final String HDR = '''
         import groovy.transform.TypeChecked
+        import groovy.transform.Pure
         import groovy.contracts.Requires
         import groovy.contracts.Ensures
         import groovy.contracts.Invariant
@@ -10034,8 +10035,8 @@ class WrapCounter implements Counter { }
                         final boolean present
                         final Object value
                         private Maybe(boolean present, Object value) { this.present = present; this.value = value }
-                        @groovy.transform.Pure static Maybe some(Object v) { new Maybe(true, v) }
-                        @groovy.transform.Pure static Maybe none() { new Maybe(false, null) }
+                        @Pure static Maybe some(Object v) { new Maybe(true, v) }
+                        @Pure static Maybe none() { new Maybe(false, null) }
                         @Requires({ f != null }) Maybe flatMap(java.util.function.Function f) { present ? (Maybe) f.apply(value) : this }
                         @Requires({ g != null }) Maybe map(java.util.function.Function g) { present ? some(g.apply(value)) : this }
                         static Maybe addPair() { DO(a in some(2), b in some(3)) { some(((Integer) a) + ((Integer) b)) } } }'''],
@@ -10045,8 +10046,8 @@ class WrapCounter implements Counter { }
                         final boolean present
                         @NonNull final Object value
                         private Maybe(boolean present, Object value) { this.present = present; this.value = value }
-                        @groovy.transform.Pure static Maybe some(Object v) { new Maybe(true, v) }
-                        @groovy.transform.Pure static Maybe none() { new Maybe(false, null) }
+                        @Pure static Maybe some(Object v) { new Maybe(true, v) }
+                        @Pure static Maybe none() { new Maybe(false, null) }
                         @Requires({ f != null }) Maybe flatMap(java.util.function.Function f) { present ? (Maybe) f.apply(value) : this }
                         @Requires({ g != null }) Maybe map(java.util.function.Function g) { present ? (g.apply(value) == null ? none() : some(g.apply(value))) : this } }'''],
 
@@ -10117,7 +10118,7 @@ class Buffer {
     }
 }''')],
         [group: 'P-fourchecker', name: 'README: Maybe under four checkers + DO', ok: true,
-         src: HDR + 'import groovy.transform.Monadic\nimport groovy.transform.Pure\nimport java.util.function.Function\n' + '''@Monadic(bind = 'flatMap', map = 'map')
+         src: HDR + 'import groovy.transform.Monadic\nimport java.util.function.Function\n' + '''@Monadic(bind = 'flatMap', map = 'map')
 @TypeChecked(extensions = ['groovy.typecheckers.NullChecker', 'groovy.typecheckers.MonadicChecker',
                            'groovy.typecheckers.PurityChecker', 'verification.VerifyChecker'])
 class Maybe {                                              // a hand-rolled Some(value) | None
